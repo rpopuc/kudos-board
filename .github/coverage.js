6b9d3c57ee3ef44c8629d6c2872f8bfc012666b8
Robson P Oliveira <rpopuc@gmail.com>
2023-04-24 08:58:49 -0300

    refact: apply code style to coverage.js

--- .github/coverage.js
+++ .github/coverage.js
@@ -1,19 +1,19 @@
 const threshold = {
-    statements: 100,
-    branches: 100,
-    functions: 100,
-    lines: 100
+  statements: 100,
+  branches: 100,
+  functions: 100,
+  lines: 100
 }
 
 const fs = require('fs')
 const coverage = JSON.parse(fs.readFileSync('./tests/coverage/coverage-summary.json')).total
 const failed = Object.keys(threshold).some(key => {
-    return coverage[key].pct < threshold[key]
+  return coverage[key].pct < threshold[key]
 })
 if (failed) {
-    console.log("Failed!")
-    console.log(coverage)
-    process.exit(1)
+  console.log("Failed!")
+  console.log(coverage)
+  process.exit(1)
 } else {
-    console.log("Coverage success!")
+  console.log("Coverage success!")
 }
