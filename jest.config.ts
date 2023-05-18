5d73453604dbb4c91d18cf86b27eaa9a69f79c0e
Arthur Schulle Konrad <arthurskonrad@gmail.com>
2023-05-18 08:19:30 -0300

    chore(jest): setup coverage

--- jest.config.ts
+++ jest.config.ts
@@ -34,12 +34,10 @@ export default {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    "html"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
