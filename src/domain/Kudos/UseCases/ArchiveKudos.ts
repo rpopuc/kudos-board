c1edf39ed31d710ff1574e316102a5a093a7740b
Arthur Schulle Konrad <arthurskonrad@gmail.com>
2023-06-29 08:56:06 -0300

    feat(Kudos): add archive operation

---
+++ src/domain/Kudos/UseCases/ArchiveKudos.ts
@@ -0,0 +1,41 @@
+import Repository from "@/domain/Kudos/Repositories/KudosRepository";
+import ArchiveKudosResponse from "@/domain/Kudos/UseCases/Responses/ArchiveKudosResponse";
+import ArchiveKudosErrorResponse from "@/domain/Kudos/UseCases/Responses/ArchiveKudosErrorResponse";
+import BusinessError from "@/domain/shared/errors/BusinessError";
+
+export type ArchiveKudosData = {
+  slug: string;
+  userId: string;
+};
+
+class ArchiveKudos {
+  constructor(private repository: Repository) {}
+
+  async handle({ slug, userId }: ArchiveKudosData): Promise<ArchiveKudosResponse> {
+    const kudos = this.repository.findBySlug(slug);
+
+    if (!kudos) {
+      return new ArchiveKudosErrorResponse([
+        new BusinessError("KUDOS_NOT_FOUND", "You can not archive a kudos that does not exist."),
+      ]);
+    }
+
+    if (userId !== kudos.from.id) {
+      return new ArchiveKudosErrorResponse([
+        new BusinessError("NOT_AUTHORIZED", "You can not archive a kudos that is not yours."),
+      ]);
+    }
+
+    const operationResult = this.repository.archive(slug);
+
+    if (!operationResult) {
+      return new ArchiveKudosErrorResponse([
+        new BusinessError("KUDOS_NOT_ARCHIVED", "Internal error while archiving the kudos."),
+      ]);
+    }
+
+    return new ArchiveKudosResponse(operationResult);
+  }
+}
+
+export default ArchiveKudos;
---
+++ tests/domains/Kudos/useCases/ArchiveKudos.test.ts
@@ -0,0 +1,86 @@
+import PanelEntity, { Status } from "@/domain/Kudos/Entities/Kudos";
+import KudosRepository from "@/domain/Kudos/Repositories/KudosRepository";
+import KudosData from "@/domain/Kudos/DTO/KudosData";
+import KudosEntity from "@/domain/Kudos/Entities/Kudos";
+import ArchiveKudos from "@/domain/Kudos/UseCases/ArchiveKudos";
+import ArchiveKudosErrorResponse from "@/domain/Kudos/UseCases/Responses/ArchiveKudosErrorResponse";
+
+describe("ArchiveKudos", () => {
+  let mockedRepository: KudosRepository;
+  let kudosData: KudosData;
+  let kudos: KudosEntity;
+
+  beforeEach(() => {
+    kudosData = {
+      title: "Example Title",
+      description: "",
+      from: { id: "user-id", name: "User 1" },
+      to: "User 2",
+      panelSlug: "panel-slug-1",
+    } as KudosData;
+
+    kudos = new KudosEntity(kudosData);
+
+    mockedRepository = {
+      create: jest.fn(),
+      update: jest.fn(),
+      archive: jest.fn(),
+      delete: jest.fn(),
+      findBySlug: jest.fn(),
+    } as KudosRepository;
+  });
+
+  test("should archive an existing kudos successfully", async () => {
+    jest.spyOn(mockedRepository, "findBySlug").mockImplementation(() => kudos);
+    jest.spyOn(mockedRepository, "archive").mockImplementation(() => true);
+
+    const archivePanel = new ArchiveKudos(mockedRepository);
+
+    const operationResponse = await archivePanel.handle({ slug: kudos.slug, userId: "user-id" });
+
+    expect(operationResponse.ok).toBe(true);
+  });
+
+  test("should not be able to archive a non existing kudos", async () => {
+    jest.spyOn(mockedRepository, "findBySlug").mockImplementation(() => null);
+    const archive = jest.spyOn(mockedRepository, "archive");
+
+    const archivePanel = new ArchiveKudos(mockedRepository);
+
+    const operationResponse = await archivePanel.handle({ slug: kudos.slug, userId: "user-id" });
+
+    expect(operationResponse.ok).toBe(false);
+    expect(operationResponse.errors[0].status).toBe("KUDOS_NOT_FOUND");
+    expect(operationResponse.errors[0].message).toBe("You can not archive a kudos that does not exist.");
+    expect(archive).not.toHaveBeenCalled();
+  });
+
+  it("should return an error if the user is not the kudos's owner", async () => {
+    jest.spyOn(mockedRepository, "findBySlug").mockImplementation(() => kudos);
+    const archive = jest.spyOn(mockedRepository, "archive");
+
+    const archivePanel = new ArchiveKudos(mockedRepository);
+
+    const operationResponse = await archivePanel.handle({ slug: kudos.slug, userId: "invalid-user-id" });
+
+    expect(operationResponse.ok).toBe(false);
+    expect(operationResponse).toBeInstanceOf(ArchiveKudosErrorResponse);
+    expect(operationResponse.errors[0].status).toBe("NOT_AUTHORIZED");
+    expect(operationResponse.errors[0].message).toBe("You can not archive a kudos that is not yours.");
+    expect(archive).not.toHaveBeenCalled();
+  });
+
+  it("should return an error if the archive action failed", async () => {
+    jest.spyOn(mockedRepository, "findBySlug").mockImplementation(() => kudos);
+    jest.spyOn(mockedRepository, "archive").mockImplementation(() => false);
+
+    const archivePanel = new ArchiveKudos(mockedRepository);
+
+    const operationResponse = await archivePanel.handle({ slug: kudos.slug, userId: "user-id" });
+
+    expect(operationResponse.ok).toBe(false);
+    expect(operationResponse).toBeInstanceOf(ArchiveKudosErrorResponse);
+    expect(operationResponse.errors[0].status).toBe("KUDOS_NOT_ARCHIVED");
+    expect(operationResponse.errors[0].message).toBe("Internal error while archiving the kudos.");
+  });
+});
