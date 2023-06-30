9a7a181f4ac3199aa4259e7fac283bf2813c0972
Robson P Oliveira <rpopuc@gmail.com>
2023-06-30 08:12:02 -0300

    wip

---
+++ src/infra/MongoDB/models/game.ts
@@ -0,0 +1,10 @@
+import { ObjectId } from "mongodb";
+
+export default class Game {
+  constructor(
+    public name: string,
+    public price: number,
+    public category: string,
+    public id?: ObjectId
+  ) {}
+}
